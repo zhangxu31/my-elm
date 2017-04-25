feb6fe66523953ff0d5185004cdbf00dedb02625
zhangxu31 <zhangxu31@zx.local>
2017-04-25 17:12:37 +0800

    add all

--- src/main.js
+++ src/main.js
@@ -4,12 +4,17 @@ import 'babel-polyfill';
 
 import Vue from 'vue';
 import router from './router';
-import app from './app.vue';
+// import app from './app.vue';
 import store from './vuex';
 
-new Vue({
+/* new Vue({
     el: '#app',
     router,
     store,
     render: h => h(app)
-});
+}); */
+
+new Vue({
+    router,
+    store
+}).$mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import App from '../App.vue';
 
 Vue.use(Router);
 
 export default new Router({
-    routes: [{ path: '/' }]
+    routes: [{
+        path: '/',
+        component: App
+    }]
 });
